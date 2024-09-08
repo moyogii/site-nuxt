401603d014a110adbbb01fa7967039d528a55f38
Moyogi <22126651+moyogii@users.noreply.github.com>
2024-09-08 00:48:13 -0500

    :art: Update seo tags & package app

--- app.vue
+++ app.vue
@@ -2,6 +2,17 @@
   <NuxtPage />
 </template>
 
+<script setup lang="ts">
+useSeoMeta({
+  title: "Moyogi's Portfolio",
+  ogTitle: "Moyogi's Portfolio",
+  description: "A portfolio website showcasing Moyogi's projects and experience.",
+  ogDescription: "A portfolio website showcasing Moyogi's projects and experience.",
+  ogImage: 'https://moyogi.dev/img/social_card.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
+
 <style lang="scss">
   @import url('https://fonts.googleapis.com/css?family=Quicksand:400,600,700&display=swap');
   body {
